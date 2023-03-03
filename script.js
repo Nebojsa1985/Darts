cbc09f81f0dd64be72d145da8e9d51ddcfc985d5
Nebojša <jnesa85@gmail.com>
2023-03-03 13:11:00 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -7,8 +7,13 @@ const addPlayerInput = document.querySelector('#addPlayerInput')
 const playersList = document.querySelector('#playersList')
 
 const startGameBtn = document.querySelector('#startGameBtn')
+const screen = document.querySelector('.screen')
+const playerboard = document.querySelector('.playerboard') 
 
 let players = []
+let activePlayerIndex = 0
+let playerOnMove
+
 
 //add players
 addPlayerBtn.addEventListener('click', ()=>{
@@ -20,7 +25,7 @@ addPlayerBtn.addEventListener('click', ()=>{
 } else if(addPlayerInput.value.length > 8){
   alert('Max 8 characters name')
 } else {
-  players.push(addPlayerInput.value)
+  players.push({name: addPlayerInput.value, p15 : [], p16 : [], p17 : [], p18 : [], p19 : [], p20 : [], pBull : []})
   let newPlayerDiv = document.createElement('h4')
   newPlayerDiv.innerHTML = `${addPlayerInput.value}`
   newPlayerDiv.id = `${addPlayerInput.value}`
@@ -39,6 +44,25 @@ addPlayerBtn.addEventListener('click', ()=>{
   addPlayerInput.focus()
 }
 })
+//make table
+function makeTable() {
+  let table = '<table border="1">';
+  table += `<tr><th>Name</th><th>15</th><th>16</th><th>17</th><th>18</th><th>19</th><th>20</th><th>Bull</th></tr>`;
+  players.forEach(player => {
+    table = table + `<tr>`;
+    table = table + `<td>${player.name}</td>`;
+    table = table + `<td>${player.p15}</td>`;
+    table = table + `<td>${player.p16}</td>`;
+    table = table + `<td>${player.p17}</td>`;
+    table = table + `<td>${player.p18}</td>`;
+    table = table + `<td>${player.p19}</td>`;
+    table = table + `<td>${player.p20}</td>`;
+    table = table + `<td>${player.pBull}</td>`;  
+    table += `</tr>`;
+  });
+  table += "</table>";
+  document.querySelector(".scoreboard").innerHTML = table;
+}
 
 //start game
 startGameBtn.addEventListener('click', ()=>{
@@ -46,6 +70,11 @@ startGameBtn.addEventListener('click', ()=>{
     alert('Must add 2 players')
   } else {
     console.log('START!!!!!!!!!!!');
+
+    makeTable()
+    screen.innerHTML = `${players[0].name} move`
+    playerOnMove = players[activePlayerIndex].name
+
     homepage.style.display = 'none'
     document.body.style.backgroundImage = 'none'
     scoreboard.style.display = 'block'
@@ -58,10 +87,58 @@ startGameBtn.addEventListener('click', ()=>{
 
 //game
 const allfields = document.querySelectorAll('.dartsfield')
-
+let allHits = []
 for (let i = 0; i < allfields.length; i++) {
     allfields[i].addEventListener('click', (e) => {
-      console.log(e.target.id);
+      playerboard.innerHTML = ''
+      console.log(playerOnMove);
+      console.log(e.target.id);   
+      let hitValue = e.target.id
+      
+      allHits.push(hitValue)
+      if(allHits.length == 4) {
+
+        // if(allHits.includes('50')) {
+        //   players[activePlayerIndex].pBull.push('.')
+        //   makeTable()
+        // }
+
+        const counts = {};
+        
+        allHits.forEach(function (x) { counts[x] = (counts[x] || 0) + 1; });
+        players[activePlayerIndex].pBull.push(counts['50'])
+        makeTable()
+        
+     
+
+
+        
+        hitValue = '🎯🎯🎯🎯'
+        alert('next player')      
+        allHits = []
+        activePlayerIndex < players.length-1 ? activePlayerIndex += 1 : activePlayerIndex = 0
+        
+        playerOnMove = players[activePlayerIndex].name
+        screen.innerHTML = `${players[activePlayerIndex].name} move`
+      }
+    
+
+      const playermove = document.createElement('p')
+
+      let removeMove = document.createElement('span')
+      removeMove.innerHTML = '&nbsp❎'
+      removeMove.classList.add('remove-move')
+      removeMove.addEventListener('click', (e)=>{
+        e.target.parentElement.remove()
+        hitValue = null
+        allHits.pop()
+      })
+
+      playermove.innerHTML = `${playerOnMove} - ${hitValue}`
+      playermove.append(removeMove)
+      playerboard.append(playermove)
+      console.log(allHits);
     })
   
 }
+
